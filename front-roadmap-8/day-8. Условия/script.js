98af7a8b632fc45ae51f76c6e317878daae1a598
Дима <jedalus@yandex.ru>
2024-05-21 18:35:15 +0500

    Прошел урок по сравнениям.
    Всё довольно логично:
    - цифарки сравниваются с цифарками,
    - строковые переменные сравниваются посимвольно. Мелкие сиволы больше крупных, если что (а>А и а>Я, но я>а)
    - при сравнении строк и цифарок попытаемся превратить строку в цифру. Если преобразовать строку не получилось, получим NaN, а сравнение с ним всегда даёт false;
    
    Вобщем, просто всегда используем строгое сравнение и радуемся жизни.

--- front-roadmap-8/day-8. Условия/script.js
+++ front-roadmap-8/day-8. Условия/script.js
@@ -1,95 +1,79 @@
-// Тут объявлены переменные
+let a = 5;
+let b = 8;
 
-// ИМЕНА ПЕРЕМЕННЫХ
-// Не могут начинаться с цифр
-// Должны содержать только буквы, цифры, '_' и '$'
+console.log(a > b);
+console.log(a < b);
 
-// Создаем с помощью CamelCase (userName)
-// Без уточнения типа данных (userName, а не userNameString)
-// Соблюдаем регистр (user и USER — это разные переменные)
+if (a > b) {
+    console.log(`${a} is more than ${b}!`);
+} else {
+    console.log(`Wow, surprisingly ${b} is more than ${a}`);
+}
 
-// Должно быть читаемым (не uname, а userName)
-// Отражать суть, но без подробностей (не namePlusSurName, а fullName)
-// Быть недвусмысленным (не summ, а itemsCount)
+// При сравнении значения будут приводиться к Number
+console.log('5' == 5);
+console.log('5' > 3);
+console.log('5' < 4);
 
-// Не называем переменные:
-// Случайными символами (123, abc, gege23e2)
-// Очевидными словами (name, variable)
-// В других стилях (kebab-case, snake_case)
-// Не латиницей (имяПользователя)
+//Но это именно при сравнении! Например, Boolean приводит 0 к False, а '0' к true (типа не пустая строка).
+// В этот момент происходит магия
+a = 0;
+b = '0';
 
-let userName = 'Dmitriy';
-let username = 'Oleg';
-let userAge = 30 + 1;
-let undfnd;
-let nll = null;
+console.log(a == b); //При приведении к Number значения этих переменных равны
+console.log(Boolean(a)); //При приведении к Boolean a == false, b == true
+console.log(Boolean(b));
 
-console.log(`${userName} и ${username} пошли играть в прятки`);
+//А еще такое сравнение считает 0 и false одним и тем же
+console.log(0 == false);
 
-// Тестируем вывод. Интересно, что сложение двух значений будет расценено, как отдельные символы '1+8', а не сумма.
-// Эту штуку лечат скобки (2+3). А вот деление сработает адекватно и без скобок 3/2 (еще и дробью будет)
-console.log(
-    userName +
-        ' уже ' +
-        userAge +
-        ' год ' +
-        (2 + 3) +
-        ' дней ' +
-        1 +
-        8 +
-        ' минут и ' +
-        3 / 2 +
-        ' секунд живёт на этой планете'
-);
+// Поэтому такое сравнение называют «нестрогим» (оно и вправду совсем не строгое)
 
-// Еще немного тестируем вывод. Тут неожиданностью стало null + число.
-// А также, что все значения переменных превращаются в строку, если к ним прибавить строку (даже undefined)
-// Причем, если записано 1+1+'3'+4, то получится '234'. Т.е. мы сперва сложим числа, а при прибавлении строки превратим все данные в строку и дальше будем работать со строчной переменной
-console.log('----------------------------');
-console.log(5 - 'a');
-console.log(0 - 'a');
-console.log('0' - 'a');
-console.log(0 + 'a');
-console.log(+Infinity - 'a');
-console.log(+Infinity + 'a');
-console.log(+Infinity + 3);
-console.log(NaN + 3);
+////////////////////////////////////
 
-console.log('----------------------------');
-console.log(undfnd + 3);
-console.log(undfnd + '3');
-console.log(undfnd);
+// Существует «строгое сравнение». Оно обозначается '===' и строго проверяет соответствие типов
+// Ну и не забываем про «нестрогое» и «строгое» неравенства '!=' и '!=='
+a = 0;
+b = '0';
 
-console.log('----------------------------');
-console.log(nll + 3);
-console.log(nll + '3');
-console.log(nll);
-console.log(3 / 0);
+console.log(a === b); //false
+console.log(0 === false); //false
+console.log(a === 0); //true
+console.log(b === '0'); //true
 
-console.log('----------------------------');
-console.log(`2 + 2 = ${2 + 2}`);
-console.log(`5+7` + '5');
-// Не знал про обратные скобки. Они как обычные, но знак доллара и фигурные скобки творят чудеса
-console.log(`${userAge} + $ + {}` + '5');
-userName = 8;
-console.log(userName + 1);
+//Как всегда прекрасная магия с null и undefined:
+//При нестрогом равенстве они равны друг другу и не равны больше ничему
+//При строгом равенстве они не равны друг другу
+//НО! При использовании математических операндов и ДРУГИХ операндов сравнения null и undefined приводятся к 0 и NaN соответсвенно.
 
-// КОНСТАНТЫ
-// Задаем, когда есть строго определенное значение, которое никогда не должно меняться
-// Это может быть, например, кодировка цвета
+console.log(null == null); //true
+console.log(undefined == undefined); //true
+console.log(null == undefined); //true
 
-const my_birthday = '04.11.1992';
-const COLOR_MAIN = '#F00';
-console.log(COLOR_MAIN);
+console.log(null === null); //true
+console.log(undefined === undefined); //true
+console.log(null === undefined); //false
 
-// Именуем константы с большой буквы, если их значения известны до выполнения программы.
-// Именуем константы с маленькой, если определим их значения в процессе.
+console.log(null == 0); //false
+console.log(null > 0); //false
+console.log(null < 0); //false
+console.log(null >= 0); //true
+console.log(null <= 0); //true
 
-console.log(Number(0.1) + Number(0.2));
-console.log('b' + 'a' + +'a' + 'a');
-console.log(typeof typeof 123);
-console.log([] + false - null + true);
-console.log(+[] + false - null + true);
+// При любом сравнении с undefined всегда получаем false. Кроме нестрогого сравнения с null
+console.log(undefined == null); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+console.log(undefined >= 0); //false
+console.log(undefined <= 0); //false
 
-console.log('baNaNa'.toLowerCase);
-console.log('baNaNa'.toLowerCase);
+console.log(Number(null)); //0
+console.log(Number(undefined)); //NaN
+
+5 > 4; // true
+'ананас' > 'яблоко'; // false
+'2' > '12'; //true
+undefined == null; //true
+undefined === null; //false
+null == '\n0\n'; //false
+null === +'\n0\n'; //false
